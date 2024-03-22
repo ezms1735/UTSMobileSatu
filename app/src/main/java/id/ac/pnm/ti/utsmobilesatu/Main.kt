87eb824ab2136249a7b0dcd9cc2dc961660fb7c3
Ergi Zenila Marta Sasmige <ergizms@gmail.com>
2024-03-22 11:10:56 +0700

    Tugas UTS Mobile 1

--- app/src/main/java/id/ac/pnm/ti/utsmobilesatu/Main.kt
+++ app/src/main/java/id/ac/pnm/ti/utsmobilesatu/Main.kt
@@ -1,6 +1,6 @@
 package id.ac.pnm.ti.utsmobilesatu
-fun main() {
-    // Inisialisasi mutableList dan tambah data List
+
+fun main () {
     val clubs = mutableListOf(
         Club("Liverpool", 19, 8, 9, 6, 3),
         Club("Manchester United", 20, 12, 5, 3, 1),
@@ -8,56 +8,42 @@ fun main() {
         Club("Manchester City", 8, 8, 8, 0, 0),
         Club("Arsenal", 13, 14, 2, 0, 0),
     )
-    println("============================================================================================")
-    // Pengurutan klub berdasarkan total trofi dari yang terbanyak ke tersedikit
-    val sortedClubs = filterAndSort(clubs) { sortByNumberTrophyTotal(it) }
+    println("==============================================================================")
+        val sortedClubs = filterAndSort(clubs) {sortByNumberTrophyTotal(it)}
 
-    // Print nama klub dan total trofi
-    println("Urutan klub berdasarkan jumlah total trofi: ")
-    for (club in sortedClubs) {
-        println("${club.name}: ${club.totalTrophy}")
-    }
-    println("============================================================================================")
+        println("Urutan klub berdasarkan jumlah total trofi: ")
+        for (club in sortedClubs) {
+            println("${club.name}: ${club.totalTrophy}")
+        }
+    println("==============================================================================")
+        val filteredClubs = filterAndSort(clubs) {filterWithoutUCLUEL(it)}
 
-    // Filter clubs without UCL and UEL wins
-    val filteredClubs = filterAndSort(clubs) { filterWithoutUCLUEL(it) }
-
-    // Print club name only for filtered clubs
-    println("Klub yang belum pernah memenangkan UCL dan UEL:")
-    for (club in filteredClubs) {
-        println(club.name)  // Print only club name
-    }
-    println("============================================================================================")
-    val liverpool = Club(
-        name = "Liverpool",epl = 19, fa = 8, efl = 9, ligaChampions = 6,ligaEuropa = 3
-    )
-// Gunakan fungsi joinToString untuk menggabungkan daftar string menjadi satu kalimat
-    val recap = listOf(
-        "${liverpool.epl} trofi EPL",
-        "${liverpool.fa} trofi FA",
-        "${liverpool.efl} trofi EFL",
-        "${liverpool.ligaChampions} trofi UCL",
-        "${liverpool.ligaEuropa} trofi UEL"
-    ).joinToString(", ")
-
-    println("Liverpool berhasil meraih $recap.")
-
-
+        println("Klub yang belum pernah memenangkan UCL dan UEL: ")
+        for (club in filteredClubs) {
+            println(club.name)
+        }
+    println("===============================================================================")
+        val liverpool = Club(
+            name = "Liverpool", epl = 19, fa = 8, efl = 9, ligaChampions = 6, ligaEuropa = 3
+        )
+        val recap = listOf(
+            "${liverpool.epl} trofi EPL",
+            "${liverpool.fa} trofi FA",
+            "${liverpool.efl} trofi EFL",
+            "${liverpool.ligaChampions} trofi UCL",
+            "${liverpool.ligaEuropa} trofi UEL"
+        ).joinToString {", "}
+        println("Liverpool berhasil meraih $recap")
 }
 
-// High order function filter and sort
 fun filterAndSort(clubs: List<Club>, options: (List<Club>) -> List<Club>): List<Club> {
     return options(clubs)
 }
-
-// Fungsi sorting
 fun sortByNumberTrophyTotal(clubs: List<Club>): List<Club> {
-    val sortedClubs = clubs.sortedByDescending { it.totalTrophy }
+    val sortedClubs = clubs.sortedByDescending {it.totalTrophy}
     return sortedClubs
 }
-
-// Fungsi filter (new) - filter clubs without UCL and UEL wins
 fun filterWithoutUCLUEL(clubs: List<Club>): List<Club> {
-    val filteredClubs = clubs.filter { it.ligaChampions == 0 && it.ligaEuropa == 0 }
+    val filteredClubs = clubs.filter {it.ligaChampions == 0 && it.ligaEuropa == 0}
     return filteredClubs
 }
